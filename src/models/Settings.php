d29f43bfc924682a2d1fd18fc8a9b83ac316a941
roy <roy@appmanschap.nl>
2024-08-08 10:30:49 +0200

    Added base plugin files

---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace appmanschap\youtubeplaylistimporter\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Youtube Playlist Importer settings
+ */
+class Settings extends Model
+{
+}
