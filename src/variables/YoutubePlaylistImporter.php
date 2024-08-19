0b1385c8d69947833810e25550d9eee7735e2549
almeric <almeric@appmanschap.nl>
2024-08-19 13:45:39 +0200

    Allow querying videos & playlists
    E.g. `{% set video = craft.ypi.videos.id(123548).one() %}`

--- src/variables/YoutubePlaylistImporter.php
+++ src/variables/YoutubePlaylistImporter.php
@@ -6,25 +6,40 @@ use appmanschap\youtubeplaylistimporter\elements\db\PlaylistQuery;
 use appmanschap\youtubeplaylistimporter\elements\db\VideoQuery;
 use appmanschap\youtubeplaylistimporter\elements\Playlist;
 use appmanschap\youtubeplaylistimporter\elements\Video;
+use Craft;
 use yii\base\InvalidConfigException;
 
 class YoutubePlaylistImporter
 {
+
     /**
-     * @return PlaylistQuery<array-key, Playlist>
+     * @param array $criteria
+     * @return PlaylistQuery
+     * @throws InvalidConfigException
      */
-    public function playlists(): PlaylistQuery
+    public function playlists(array $criteria = []): PlaylistQuery
     {
-        return Playlist::find();
+        $query =  Playlist::find();
+        if ($criteria) {
+            Craft::configure($query, $criteria);
+        }
+
+        return $query;
     }
 
     /**
-     * @param array<array-key, string> $tags
-     * @return VideoQuery<array-key, Video>
+     * @param array $criteria
+     * @return VideoQuery
      * @throws InvalidConfigException
      */
-    public function videos(array $tags = []): VideoQuery
+    public function videos(array $criteria = []): VideoQuery
     {
-        return Video::find()->tags($tags);
+        $query = Video::find();
+
+        if ($criteria) {
+            Craft::configure($query, $criteria);
+        }
+
+        return $query;
     }
 }
