bc7f6b4fd613dfbd611164a481ba98a5ce96b5ee
almeric <almeric@appmanschap.nl>
2024-08-21 11:47:47 +0200

    Embeddable
    - Allow querying videos' embeddable field
    - Add element condition for videos' embeddable field

--- src/elements/conditions/VideoCondition.php
+++ src/elements/conditions/VideoCondition.php
@@ -2,17 +2,22 @@
 
 namespace appmanschap\youtubeplaylistimporter\elements\conditions;
 
+use appmanschap\youtubeplaylistimporter\elements\conditions\rules\EmbeddableConditionRule;
 use craft\elements\conditions\ElementCondition;
+use craft\errors\InvalidTypeException;
 
 /**
  * Video condition
  */
 class VideoCondition extends ElementCondition
 {
+    /**
+     * @throws InvalidTypeException
+     */
     protected function selectableConditionRules(): array
     {
         return array_merge(parent::selectableConditionRules(), [
-            // ...
+            EmbeddableConditionRule::class,
         ]);
     }
 }
---
+++ src/elements/conditions/rules/EmbeddableConditionRule.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace appmanschap\youtubeplaylistimporter\elements\conditions\rules;
+
+use appmanschap\youtubeplaylistimporter\elements\db\VideoQuery;
+use appmanschap\youtubeplaylistimporter\elements\Video;
+use Craft;
+use craft\base\conditions\BaseLightswitchConditionRule;
+use craft\base\ElementInterface;
+use craft\elements\conditions\ElementConditionRuleInterface;
+use craft\elements\db\ElementQueryInterface;
+
+class EmbeddableConditionRule extends BaseLightswitchConditionRule implements ElementConditionRuleInterface
+{
+    public function getLabel(): string
+    {
+        return Craft::t('youtube-playlist-importer', 'Embeddable');
+    }
+
+    public function getExclusiveQueryParams(): array
+    {
+        return ['embeddable'];
+    }
+
+    public function modifyQuery(ElementQueryInterface $query): void
+    {
+        /** @var VideoQuery<array-key, Video> $query */
+        $query->embeddable($this->value);
+    }
+
+    public function matchElement(ElementInterface $element): bool
+    {
+        /** @var Video $element */
+        return $this->matchValue($element->embeddable);
+    }
+}
